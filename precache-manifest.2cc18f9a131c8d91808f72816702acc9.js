95ca83fdc4507490e0db6085f8ae1540a2dbd3cb
Bernardo Nascimento <bernardonasciimento@gmail.com>
2020-01-12 03:35:47 -0300

    Updates

---
+++ precache-manifest.2cc18f9a131c8d91808f72816702acc9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ee05212503750991eb52921dcaf08fb6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "82b7e003870d2e818f80",
+    "url": "/static/css/main.8af9e8ae.chunk.css"
+  },
+  {
+    "revision": "0c560a250700cc7c7d17",
+    "url": "/static/js/2.2fb6e166.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.2fb6e166.chunk.js.LICENSE"
+  },
+  {
+    "revision": "82b7e003870d2e818f80",
+    "url": "/static/js/main.e53bc75e.chunk.js"
+  },
+  {
+    "revision": "a20db87df2393cd42649",
+    "url": "/static/js/runtime-main.4fbf9490.js"
+  }
+]);
